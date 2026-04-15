b10957be9634ef3beaf3a2a336d0c30f645ba1ea
BeeBeard <beebeard@yandex.ru>
2026-04-15 17:06:04 +0300

    багфикс прокси

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -5,7 +5,7 @@ import json
 import re
 from dataclasses import dataclass
 from typing import Union
-
+from aiohttp import ClientSession
 import requests
 from aiogram import Bot
 from aiogram.client.bot import DefaultBotProperties
@@ -31,8 +31,8 @@ class BotData:  # Данные бота
     add_url: str = ""
     session = None
 
-    def __init__(self, token: str = None):
-
+    def __init__(self, token: str = None, proxy: str = "socks5://F7f74d:6hxDPb@45.157.123.53:8000"):
+        self.proxy = proxy
         self.token: str = token
 
     async def init(self):
@@ -44,17 +44,13 @@ class BotData:  # Данные бота
                 return await self.set_bot()
         raise ValueError(f"Не удалось запустить бота")
 
-
-
     def check_token(self):
         if self.token and re.findall(r'^\d{10}:[\w\W]{35}', self.token):
             return True
         return False
 
     async def get_session(self):
-        proxy = "socks5://F7f74d:6hxDPb@45.157.123.53:8000"
-        logger.debug(f"{proxy=}")
-        self.session = AiohttpSession(proxy=proxy)
+        self.session = AiohttpSession(proxy=self.proxy)
         return self.session
 
     async def set_bot(self):
@@ -69,15 +65,19 @@ class BotData:  # Данные бота
         return self
 
     async def get_info(self):
+        async with ClientSession() as session:
+            async with session.get(
+                f"https://api.telegram.org/bot{self.token}/getMe",
+                proxy=self.proxy
+            ) as resp:
+                content = await resp.json()
 
-        url = f"https://api.telegram.org/bot{self.token}/getMe"
-        result = requests.get(url=url, proxies=self.session.proxy)
-        content = json.loads(result.content.decode('utf8'))
-        self.title = content["result"]["first_name"]
-        self.name = content["result"]["username"]
-        self.url = f"https://t.me/{self.name}"  # НЕ менять привязан к БОТУ!
-        self.start_url = f"{self.url}?start="
-        self.add_url = f"t.me/{self.name}?startgroup"
+                # content = json.loads(result.content.decode('utf8'))
+                self.title = content["result"]["first_name"]
+                self.name = content["result"]["username"]
+                self.url = f"https://t.me/{self.name}"  # НЕ менять привязан к БОТУ!
+                self.start_url = f"{self.url}?start="
+                self.add_url = f"t.me/{self.name}?startgroup"
 
     def set_commands(self) -> None:
         """Отобразить список команд для пользователя"""
